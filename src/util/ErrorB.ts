509e119fb252309d108c09d05f2fa46f2a4df8dc
Khalid <f5f@msn.com>
2016-10-31 09:28:05 -0700

    fix error code and messages

--- src/util/ErrorB.ts
+++ src/util/ErrorB.ts
@@ -3,16 +3,20 @@
  */
 export default class ErrorB {
 
-    public static PHONE_MISSING: string =         '{"code": "200", "msg": "Missing the phone number"}';
+    public static PHONE_MISSING: string =         '{"code":"200","msg":"Missing the phone number"}';
 
-    public static PHONE_EXIST: string =           '{"code": "201", "msg": "The phone number exist"}';
+    public static PHONE_EXIST: string =           '{"code":"201","msg":"The phone number exist"}';
 
-    public static PHONE_INVALID: string =          '{"code": "202", "msg": "The phone number is invalid"}';
+    public static PHONE_INVALID: string =          '{"code":"202","msg":"The phone number is invalid"}';
 
-    public static EMAIL_MISSING: string =         '{"code": "203", "msg": "Missing the email address"}';
+    public static EMAIL_MISSING: string =          '{"code":"203","msg":"Missing the email address"}';
 
-    public static EMAIL_EXIST: string =           '{"code": "204", "msg": "The email address exist"}';
+    public static EMAIL_EXIST: string =           '{"code":"204","msg":"The email address exist"}';
 
-    public static PASSWORD_MISSING: string =      '{"code": "205", "msg": "Missing password"}';
+    public static EMAIL_INVALID: string =         '{"code":"205","msg":"The email address is invalid"}';
+
+    public static PASSWORD_MISSING: string =      '{"code":"206","msg":"Missing password"}';
+
+    public static PASSWORD_INVALID: string =       '{"code":"207","msg":"Missing password"}';
 
 }
