5937bb81f593d301d9e84fba03afeec2c03760f4
Khalid <f5f@msn.com>
2016-10-31 06:32:04 -0700

    add create and delete method to userController

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -4,18 +4,35 @@ import User from '../models/User';
  * UserController
  */
 export class UserController {
-    constructor() {
-    }
-
-    public static create({phone, email, password, type}): Promise<any> {
+    /**
+     * create method: to create new user
+     * TODO: validation 
+     */
+    public static create(phone: string, email: string, password: string, type: string = 'cus'): Promise<any> {
         return new Promise((resolve, reject) => {
             let newuser = new User({
                 phone: phone,
                 email: email,
                 password: password,
-                type: 'cus'
+                u_type: type
             });
-            newuser.save().then(resolve).catch(reject);
+
+            newuser.save()
+            .then((user) => {
+                resolve(user);
+            }).catch((err) => {
+                reject(err);
+            });
+        });
+    }
+
+    public static delete(phone: string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            User.findOneAndRemove({phone: phone}, (err, user) => {
+                if (err) return reject(err);
+                else
+                return resolve(user);
+            });
         });
     }
 }
